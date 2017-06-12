fdd633d4f9e26388df5a167e3d1e2f9f63c8ae57
youth5201314 <1028729086@qq.com>
2017-06-12 17:32:20 +0800

    no message

--- app/src/main/java/com/youth/xf/App.java
+++ app/src/main/java/com/youth/xf/App.java
@@ -19,7 +19,7 @@ public class App extends XApplication {
 
         /**
              初始化网络请求的引擎,在这里可以一行代码切换，避免更换网络框架麻烦的问题
-             提供四种常见框架的简单案例：（你也可以按照例子自己实现）
+             提供三种常见框架的简单案例：（你也可以按照例子自己实现）
              AsyncHttpEngine、OKHttpEngine、VolleyHttpEngine
          */
         XFrame.initXHttp(new AsyncHttpEngine());
